49b1681f159160de49a3bbc554eaa14cbb025ec0
ahmedswar <ahmedswar68@gmail.com>
2024-06-02 15:58:30 +0300

    GOLABLE ERROR HANDLING MIDDLEWARE

--- server.js
+++ server.js
@@ -4,20 +4,23 @@ const morgan = require("morgan");
 
 dotenv.config({ path: "config.env" });
 const dbConnection = require("./config/database");
-const router =require("./routes/categoryRoute");
+const router = require("./routes/categoryRoute");
 
 dbConnection();
 const app = express();
-
+app.use(express.json());
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
+//ROUTES MOUNT
+app.use("/api/v1/categories", router);
 
-app.use('/api/v1/categories',router);
-
+// GOLABLE ERROR HANDLING MIDDLEWARE
+app.use((err, req, res, next) => {
+  res.status(400).json({ err });
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
